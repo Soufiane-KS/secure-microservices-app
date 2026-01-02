63f396c7c0dc5d6bcfea6cbe502937dfe4745028
Soufiane-KS <soufiane.kousta@uit.ac.ma>
2026-01-02 20:39:08 +0100

    initial project state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App({ keycloak }) {
+
+    const callProducts = async () => {
+        await keycloak.updateToken(30); // refresh token si besoin
+
+        const response = await axios.get(
+            "http://localhost:8085/products",
+            {
+                headers: {
+                    Authorization: `Bearer ${keycloak.token}`,
+                },
+            }
+        );
+
+        alert(response.data);
+    };
+
+    const callOrders = async () => {
+        await keycloak.updateToken(30);
+
+        const response = await axios.post(
+            "http://localhost:8085/orders",
+            {},
+            {
+                headers: {
+                    Authorization: `Bearer ${keycloak.token}`,
+                },
+            }
+        );
+
+        alert(response.data);
+    };
+
+    return (
+        <div style={{ padding: "30px", fontFamily: "Arial" }}>
+            <h2>React + Keycloak + API Gateway</h2>
+
+            <p>
+                 Utilisateur : <b>{keycloak.idTokenParsed?.preferred_username}</b>
+            </p>
+            <p>
+                Email : <b>{keycloak.idTokenParsed?.email}</b>
+            </p>
+            <p>
+                 Rôles :
+                <b> {keycloak.tokenParsed?.realm_access?.roles.join(", ")}</b>
+            </p>
+
+            <button onClick={callProducts}>
+                Voir Produits
+            </button>
+
+            <button onClick={callOrders} style={{ marginLeft: "10px" }}>
+                Créer Commande
+            </button>
+
+            <br /><br />
+
+            <button onClick={() => keycloak.logout()}>
+                Se déconnecter
+            </button>
+        </div>
+    );
 }
 
 export default App;
