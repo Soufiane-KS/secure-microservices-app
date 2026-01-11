a73487546f79ae908247cac65b9283294f39f586
safeone1 <safeonekan@gmail.com>
2026-01-11 21:07:29 +0100

    fix: configure tests to use H2 in-memory database and disable OAuth2 for testing

--- gateway/src/test/java/ma/enset/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/ma/enset/gateway/GatewayApplicationTests.java
@@ -2,8 +2,10 @@ package ma.enset.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class GatewayApplicationTests {
 
     @Test
